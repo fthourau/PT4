b49b284ceb0f35803a699b22bd43c4a13fccae5e
Clement Gilardy <cgilardy@info-uplinkx.iut.bx1>
2013-01-29 12:15:32 +0100

    maj

--- getopt.c
+++ getopt.c
@@ -9,7 +9,10 @@
      main (int argc, char **argv)
      {
        int c;
-     
+	   int i;
+	   char *argument;
+
+	   
        while (1)
          {
            static struct option long_options[] =
@@ -20,6 +23,8 @@
                   We distinguish them by their indices. */
                {"add",     no_argument,       0, 'a'},
                {"append",  no_argument,       0, 'b'},
+               {"aide",  no_argument,       0, 'h'},
+               {"verb",  no_argument, 0, 'v'},
                {"delete",  required_argument, 0, 'd'},
                {"create",  required_argument, 0, 'c'},
                {"file",    required_argument, 0, 'f'},
@@ -28,13 +33,13 @@
            /* getopt_long stores the option index here. */
            int option_index = 0;
      
-           c = getopt_long (argc, argv, "abc:d:f:",
+           c = getopt_long (argc, argv, "abvhc:d:f:",
                             long_options, &option_index);
      
            /* Detect the end of the options. */
            if (c == -1)
              break;
-     
+		   
            switch (c)
              {
              case 0:
@@ -66,6 +71,22 @@
              case 'f':
                printf ("option -f with value `%s'\n", optarg);
                break;
+               
+             case 'h':
+				if(long_options[i].has_arg == 0)
+					argument = "ne prend aucun argument";
+				else
+					argument = "un argument est requis";
+					
+				for(i = 0 ; i < 8 ; i++){
+					char *valeur1 = (char)long_options[i].val;
+					printf("Option : %s, %s,  valeur %s \n",long_options[i].name,argument,valeur1);	
+				}
+             break;
+              
+             case 'v':
+				puts("option -v \n");
+             break;
      
              case '?':
                /* getopt_long already printed an error message. */
@@ -84,7 +105,7 @@
        /* Print any remaining command line arguments (not options). */
        if (optind < argc)
          {
-           printf ("non-option ARGV-elements: ");
+           printf ("Ceci n'est pas une option: ");
            while (optind < argc)
              printf ("%s ", argv[optind++]);
            putchar ('\n');
