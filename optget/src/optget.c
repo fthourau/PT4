d9bb895ee8e3fe0a50c3f305675297a00f1b63bb
Eternel <eternel@eternel-laptop.(none)>
2013-02-13 15:54:38 +0100

    fusion

--- optget/src/optget.c
+++ optget/src/optget.c
@@ -2,32 +2,35 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include "../head/util.h"
+#include "../head/archive.h"
+#include "../head/options.h"
 
 const int NB_OPTIONS = 8; //constante qui défini le nombre d'option
-static int verbose_flag;
 
 void optget(int argc, char **argv){
 	int c; //variable du switch (elle contient les caractères d'option)s
 	int i; // variable d'une boucle dans le switch
-
+	FILE* archive = NULL;
 	while (1)
 	{
 		static struct option long_options[] =
 		{
-			{"verbose", no_argument,       &verbose_flag, 1},
-			{"add",     no_argument,       0, 'a'},
-			{"append",  no_argument,       0, 'b'},
-			{"help",  no_argument,       0, 'h'},
-			{"verb",  no_argument, 0, 'v'},
+			{"add",     no_argument, 0, 'a'},
+			{"append",  no_argument, 0, 'b'},
+			{"help",  no_argument, 0, 'h'},
+			{"verbose",  no_argument, 0, 'v'},
 			{"delete",  required_argument, 0, 'd'},
 			{"create",  required_argument, 0, 'c'},
 			{"file",    required_argument, 0, 'f'},
+			{"extract", required_argument, 0, 'x'},
+			{"list", required_argument, 0, 'l'},
+
 			{0, 0, 0, 0}
 		};
 		/* getopt_long stock l'index de l'option ici */
 		int option_index = 0;
 		
-		c = getopt_long (argc, argv, "abvhc:d:f:",
+		c = getopt_long (argc, argv, "abvhc:d:f:x:l:",
 		long_options, &option_index);
 
 		/* Detect la fin des options */
@@ -49,6 +52,10 @@ void optget(int argc, char **argv){
 				puts ("option -a\n");
 			break;
 
+			case 'v':
+				VERBOSE_FLAG = true;
+			break;
+
 			case 'b':
 				puts ("option -b\n");
 			break;
@@ -56,7 +63,22 @@ void optget(int argc, char **argv){
 			case 'c':
 				printf ("option -c with value `%s'\n", optarg);
 			break;
-
+			case 'x' :
+				archive = fopen(argv[2], "r");
+				if(archive != NULL) {
+					// extract_files_from_archive(archive);
+					extract_files_from_archive(archive);
+					fclose(archive);
+				}
+			break;
+			case 'l':
+				archive = fopen(argv[2], "r");
+				if(archive != NULL) {
+					// extract_files_from_archive(archive);
+					list_files_from_archive(archive);
+					fclose(archive);
+				}
+			break;
 			case 'd':
 				printf ("option -d with value `%s'\n", optarg);
 			break;
@@ -80,15 +102,13 @@ void optget(int argc, char **argv){
 		}
 	}
 	
-	if (verbose_flag)
-		puts ("mode verbose activer");
 
 	if (optind < argc)
 	{
-		printf ("Ceci n'est pas une option: ");
 		while (optind < argc)
 			printf ("%s ", argv[optind++]);
-		putchar ('\n');
+		printf ("n'est pas une option\n");
+		
 	}
 
 	exit (0);
